ace7869cd8783880d572da8768f1ece63679d326
Denys Zvarych <denys.zvr@gmail.com>
2022-10-24 04:16:37 +0200

    added oop in my instagram parser

--- instagram_oop.py
+++ instagram_oop.py
@@ -2,18 +2,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 import time
 from auto_data import username, password
 import pickle
 import requests
 
-
-
 class InstagramBot():
-    
 
-    def __init__(self,username, password) -> None:
+    def __init__(self, username, password):
 
         self.username = username 
         self.password = password 
@@ -21,13 +17,10 @@ class InstagramBot():
 
         #options
         options = webdriver.ChromeOptions()
-        #options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
+        options.add_argument("user-agent=Mozilla/5.0 (X11; CrOS x86_64 14816.131.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
 
-        #disable webdriver mode
-        options.add_argument("--disable-blink-features=AutomationControlled")
-
-        # chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_experimental_option("prefs", {"profile.block_third_party_cookies": True})
+        #headless
+        #options.headless = True
 
 
         self.driver = webdriver.Chrome(
@@ -40,15 +33,6 @@ class InstagramBot():
 
         self.driver.close()
         self.driver.quit()
-    
-    # def save_cookies(self):
-        
-    #     driver = self.driver
-
-    #     pickle.dump(driver.get_cookies(), open('test_cookies', 'wb'))
-
-    #     # for cookie in pickle.load(open('test_cookies', 'rb')):
-    #     #         driver.add_cookie(cookie)
 
     def login(self):
 
@@ -65,7 +49,7 @@ class InstagramBot():
         password_form = driver.find_element(By.NAME, 'password')
         password_form.send_keys(password)
         password_form.send_keys(Keys.ENTER)
-        time.sleep(7)
+        time.sleep(3)
         #self.close_browser()
 
 
@@ -74,36 +58,20 @@ class InstagramBot():
         self.login()
         account_url  = 'https://instagram.com/' + nickname
         self.driver.get(account_url)
-
-        time.sleep(2)
+        time.sleep(1)
 
         img_src = '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/header/div/div/span/img'
         account_img = self.driver.find_element(By.XPATH, img_src).get_attribute('src')
 
-
         get_img = requests.get(account_img)
-        with open(f'{nickname}_img','wb') as img_file:
+        with open(f'img_instagram/{nickname}.jpeg','wb') as img_file:
             img_file.write(get_img.content)
         
+
         self.close_browser()
 
-    #sprawdza czy element instnieje na stronie
 
-    def xpath_exists(self):
-
-        driver = self.driver
-
-        try:
-            driver.find_element(By.XPATH, self.account_url)
-            exists =  True
-        except NoSuchElementException:
-            exists = False
-
-        return exists 
-    
-
-
-my_bot = InstagramBot(username, password)
-my_bot.save_img('busterzy')
+#my_bot = InstagramBot(username, password)
+#by_bot.save_img('zvarich87')
 #my_bot.login()
 #my_bot.close_browser()
